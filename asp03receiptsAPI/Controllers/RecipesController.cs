aa56dab94e138eedf46463202195cb0149484e6e
Michal Stehlík <michal.stehlik@pslib.cz>
2025-10-06 14:13:27 +0200

    6.10.2025

---
+++ asp03receiptsAPI/Controllers/RecipesController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using asp03receiptsAPI.Data;
+using asp03receiptsAPI.Models;
+
+namespace asp03receiptsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly RecipesDbContext _context;
+        private readonly ILogger<RecipesController> _logger;
+
+        public RecipesController(RecipesDbContext context, ILogger<RecipesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Recipes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipes()
+        {
+            return await _context.Recipes.ToListAsync();
+        }
+
+        // GET: api/Recipes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetRecipe(int id)
+        {
+            var recipe = await _context.Recipes.FindAsync(id);
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return recipe;
+        }
+
+        // PUT: api/Recipes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipe(int id, Recipe recipe)
+        {
+            if (id != recipe.RecipeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Recipes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+        {
+            _context.Recipes.Add(recipe);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRecipe", new { id = recipe.RecipeId }, recipe);
+        }
+
+        // DELETE: api/Recipes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecipe(int id)
+        {
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            _context.Recipes.Remove(recipe);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpGet("{id}/ingredinces")]
+        public async Task<ActionResult<IEnumerable<Ingredient>>> GetRecipeIngredients(int id)
+        {
+            if (_context == null)
+            {
+                return BadRequest("context is null");
+            }
+            var recipe = await _context.Recipes
+                .Include(x => x.Content)
+                .ThenInclude(x => x.Ingredient)
+                .Where(x => x.RecipeId == id)
+                .Select(x => x.Content.Select(ri => ri.Ingredient))
+                .FirstOrDefaultAsync();
+            if (recipe == null)
+            {
+                return NotFound("recipe not found");
+            }
+            return Ok(recipe);
+        }
+
+        //[HttpPost("{id}/ingrediences/{ingredientId}")]
+        [HttpPost("{id}/ingrediences")]
+        public async Task<ActionResult<RecipeIngredient>> 
+            AddIngredientToRecipe(int id, [FromBody] AddIngredientToRecipeDto ing)
+        {
+            var recipe = await _context.Recipes.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound("recipe not found");
+            }
+            var ingredient = await _context.Ingredients.FindAsync(ing.IngredientId);
+            if (ingredient == null)
+            {
+                return NotFound("ingredient not found");
+            }
+            var ingredientInRecipe = await _context.Set<RecipeIngredient>()
+                .FindAsync(id, ing.IngredientId);
+            if (ingredientInRecipe != null)
+            {
+                return Conflict("ingredient already in recipe");
+            }
+
+            var recipeIngredient = new RecipeIngredient
+            {
+                RecipeId = id,
+                IngredientId = ing.IngredientId,
+                Quantity = ing.Quantity,
+                Unit = ing.Unit
+            };
+
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetRecipeIngredients", new { id = recipeIngredient.RecipeId }, recipeIngredient);
+        }
+
+        private bool RecipeExists(int id)
+        {
+            return _context.Recipes.Any(e => e.RecipeId == id);
+        }
+    }
+
+    public record AddIngredientToRecipeDto(
+        int IngredientId, 
+        int Quantity, 
+        string Unit);
+}
